6e261f0536ee174702ad2b68bf9b8165bc63eaeb
unknown <cheekongkan@hotmail.com>
2021-08-28 17:31:32 +0800

    'v0.1'

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Team 18</h1>
+        <p className="text-muted">A simple marketplace</p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
